43ea2bc3ca92c8b22d25c56ed63b7ec93b21af12
김성민 <arcn1345@gmail.com>
2024-05-16 02:58:04 +0900

    feat: Song Data class 노래 추가

--- FLO_CLONE/app/src/main/java/com/example/flo_clone/ui/song/Song.kt
+++ FLO_CLONE/app/src/main/java/com/example/flo_clone/ui/song/Song.kt
@@ -3,7 +3,8 @@ package com.example.flo_clone.ui.song
 data class Song (
     val title: String = "",
     val singer: String = "",
-    val second: Int = 0,
+    var second: Int = 0,
     val playTime: Int = 0,
-    var isPlaying: Boolean = false
+    var isPlaying: Boolean = false,
+    var music: String = ""
 )
