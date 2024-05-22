35ec47b42e6519e9026b38dd200ea59b73fe7237
김성민 <arcn1345@gmail.com>
2024-05-23 04:16:38 +0900

    feat: AlbumRvAdapter 프래그먼트와 연결

--- FLO_CLONE/app/src/main/java/com/example/flo_clone/ui/home/HomeFragment.kt
+++ FLO_CLONE/app/src/main/java/com/example/flo_clone/ui/home/HomeFragment.kt
@@ -8,11 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.flo_clone.ui.album.AlbumFragment
 import com.example.flo_clone.MainActivity
 import com.example.flo_clone.R
+import com.example.flo_clone.data.Album
 import com.example.flo_clone.databinding.FragmentHomeBinding
+import com.example.flo_clone.ui.album.AlbumRvAdapter
 import com.example.flo_clone.ui.home.banner.BannerFragment
 import com.example.flo_clone.ui.home.banner.BannerVPAdapter
 import com.example.flo_clone.ui.home.panel.PanelVPAdapter
@@ -22,6 +25,7 @@ data class SongPractice(val title: String, val singerName: String)
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding // 뷰 바인딩
+    private var albumDatas = ArrayList<Album>()
 
     var currentPage = 0
 
@@ -36,7 +40,8 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
 
-        //setOnClickView()
+        setDataList()
+        setAlbumRvAdapter()
         setVPAdapter()
         setIndicator()
 
@@ -69,6 +74,14 @@ class HomeFragment : Fragment() {
         circleIndicatior3.createIndicators(7,0)
     }
 
+    //페이지 변경하기
+    private fun setPage(){
+        if(currentPage == 7)
+            currentPage = 0
+        binding.homePanelVp.setCurrentItem(currentPage, true)
+        currentPage+=1
+    }
+
     // 타이머 스레드 실행
     private fun runTimerThread() {
         val thread = Thread(PagerRunnable())
@@ -95,28 +108,22 @@ class HomeFragment : Fragment() {
         }
     }
 
-    //페이지 변경하기
-    fun setPage(){
-        if(currentPage == 7)
-            currentPage = 0
-        binding.homePanelVp.setCurrentItem(currentPage, true)
-        currentPage+=1
+    private fun setAlbumRvAdapter() {
+
+        val albumRvAdapter = AlbumRvAdapter(albumDatas)
+        binding.homeTodayMusicAlbumRv.adapter = albumRvAdapter
+        binding.homeTodayMusicAlbumRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
     }
 
-//    private fun setOnClickView() {
-//        val albumImg = binding.albumIvImg1
-//        albumImg.setOnClickListener {
-//            val imgRes = R.drawable.lilac_album_cover
-//            val song = SongPractice(binding.tvAlbumName1.text.toString(), binding.tvAlbumSinger1.text.toString()) // 데이터 설정
-//            val albumFragment = AlbumFragment().apply {
-//                arguments = Bundle().apply {
-//                    putInt("imgRes", imgRes)
-//                    putString("title", song.title)
-//                    putString("singerName", song.singerName)
-//                }
-//            }
-//            (context as MainActivity).supportFragmentManager.beginTransaction()
-//                .replace(R.id.frame_layout, albumFragment).commitAllowingStateLoss()
-//        }
-//    }
+    private fun setDataList() {
+        albumDatas.apply {
+            add(Album("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp2))
+            add(Album("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level", "에스파 (Aespa)", R.drawable.img_album_exp2))
+            add(Album("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp2))
+            add(Album("BBoom BBoom", "모모랜드 (MomoLand)", R.drawable.img_album_exp2))
+            add(Album("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp2))
+        }
+    }
+
 }
