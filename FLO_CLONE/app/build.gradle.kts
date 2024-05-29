a70538403576196743e5bc1c11a724b97cf15d78
김성민 <arcn1345@gmail.com>
2024-05-30 04:42:26 +0900

    feat: build.gradle 수정

--- FLO_CLONE/app/build.gradle.kts
+++ FLO_CLONE/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -75,9 +76,10 @@ dependencies {
 
     // Room
     val room_version = "2.6.1"
-    implementation ("androidx.room:room-compiler:$room_version")
+    //implementation ("androidx.room:room-compiler:$room_version")
     implementation ("androidx.room:room-runtime:$room_version")
-    implementation ("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 
 
 }
