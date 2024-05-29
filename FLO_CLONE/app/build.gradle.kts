d6a889626c615a0b1ec1c90e528b7813c43c7ceb
김성민 <arcn1345@gmail.com>
2024-05-30 03:57:44 +0900

    feat: room gradle 추가

--- FLO_CLONE/app/build.gradle.kts
+++ FLO_CLONE/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.8.0")
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -71,5 +72,10 @@ dependencies {
 
     // Glide
     implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    // Room
+    implementation ("androidx.room:room-compiler:2.6.1")
+    implementation ("androidx.room:room-runtime:2.6.1")
+
 
 }
