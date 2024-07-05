1bc3b77d7ce0dcced5dac8648ac91b26d0808f4a
Seongmin <arcn1345@gmail.com>
2024-07-05 14:31:06 +0900

    feat: BaseActivity, BaseFragment 일부 코드 추가

--- FLO_CLONE/app/src/main/java/com/example/flo_clone/core/ui/base/BaseFragment.kt
+++ FLO_CLONE/app/src/main/java/com/example/flo_clone/core/ui/base/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -33,4 +34,8 @@ abstract class BaseFragment<VB : ViewBinding>(@LayoutRes private val layoutRes: 
     }
 
     abstract fun setLayout()
+
+    fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
 }
